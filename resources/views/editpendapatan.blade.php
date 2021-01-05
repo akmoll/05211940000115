129ae30fdbe345c1e3b268fc8defb827acb11a01
akmal <m.akmal3830@gmail.com>
2021-01-05 23:47:48 +0700

    Update editpendapatan.blade.php

--- resources/views/editpendapatan.blade.php
+++ resources/views/editpendapatan.blade.php
@@ -9,8 +9,9 @@
     @foreach($pendapatan as $p)
     <form action="/pendapatan/update" method="post">
         {{csrf_field()}}
+        <input type="hidden" name="id" value="{{ $p-> ID}}"> <br/>
         <div class="form-group">
-            <label id="idp">ID Pegawai</label>
+            <label">ID Pegawai</label>
             <select id="idp" name="idp" required="required" class="form-control"> <br/>
                 @foreach ($pegawai as $x)
                 <option value="{{$x -> pegawai_id}}" @if ($p->IDPegawai == $x->pegawai_id) selected="selected" @endif> {{$x -> pegawai_nama}} </option>
@@ -18,20 +19,20 @@
             </select>
          </div>
         <div class="form-group">
-            <label for="bulan">Bulan</label>
-            <input  class="form-control" id="bulan" type="number"  name="bulan" required="required" value="{{ $p->Bulan}}">
+            <label >Bulan</label>
+            <input  class="form-control"  type="number"  name="bulan" required="required" value="{{ $p->Bulan}}">
           </div>
         <div class="form-group">
-            <label for="tahun">Tahun</label>
-            <input  class="form-control" id="tahun" type="number" name="tahun" required="required" value="{{ $p->Tahun}}">
+            <label >Tahun</label>
+            <input  class="form-control"  type="number" name="tahun" required="required" value="{{ $p->Tahun}}">
           </div>
         <div class="form-group">
-            <label for="gaji">Gaji</label>
-            <input  class="form-control" id="gaji" type="number" name="gaji" required="required" value="{{ $p->Gaji}}">
+            <label >Gaji</label>
+            <input  class="form-control"  type="number" name="gaji" required="required" value="{{ $p->Gaji}}">
           </div>
         <div class="form-group">
-            <label for="tunjangan">Tunjangan</label>
-            <input  class="form-control" id="tunjangan" type="number" name="tunjangan" required="required" value="{{ $p->Tunjangan}}">
+            <label >Tunjangan</label>
+            <input  class="form-control"  type="number" name="tunjangan" required="required" value="{{ $p->Tunjangan}}">
           </div>
         <input type="submit" value="Simpan Data" class="btn btn-primary">
     </form>
